719d8f4a9ab38f85671e901c57fd8f9156c20324
mohie800 <mohy800@gmail.com>
2024-01-22 00:50:34 +0300

    persist storage

--- src/store.js
+++ src/store.js
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
 import server from "./api/server";
 
 export const useRegisterStore = create((set) => ({
@@ -14,29 +15,37 @@ export const useCityStore = create((set) => ({
   setCity: (city) => set((state) => ({ ...state, city })),
 }));
 
-const useDataStore = create((set) => ({
-  data: {
-    // logo: {},
-    services: [],
-    homeBanner: [],
-    orderBanner: [],
-    reg: {},
-    tax: {},
-    cities: [],
-  },
-  loading: true,
-  error: null,
-  setData: (data) => set((state) => ({ ...state, data })),
-  fetchData: async (url) => {
-    set({ loading: true, error: null });
-    try {
-      const { data } = await server.get("/data/get");
-      set({ data, loading: false });
-    } catch (error) {
-      set({ error });
+const useDataStore = create(
+  persist(
+    (set) => ({
+      data: {
+        // logo: {},
+        services: [],
+        homeBanner: [],
+        orderBanner: [],
+        reg: {},
+        tax: {},
+        cities: [],
+      },
+      loading: true,
+      error: null,
+      setData: (data) => set((state) => ({ ...state, data })),
+      fetchData: async (url) => {
+        set({ loading: true, error: null });
+        try {
+          const { data } = await server.get("/data/get");
+          set({ data, loading: false });
+        } catch (error) {
+          set({ error });
+        }
+      },
+    }),
+    {
+      name: "data-storage", // name of the item in the storage (must be unique)
+      storage: createJSONStorage(() => sessionStorage), // (optional) by default, 'localStorage' is used
     }
-  },
-}));
+  )
+);
 
 export const useDataFetching = () => {
   const { data, loading, error, fetchData } = useDataStore();
